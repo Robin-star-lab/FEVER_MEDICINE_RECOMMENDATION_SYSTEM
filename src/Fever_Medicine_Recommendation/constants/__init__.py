9d4afd81d5cbdeb64943cc3baf811a678ee8d3c1
Robin Aluma <alumarobin@gmail.com>
2025-03-18 10:35:45 +0300

    mongosetup changes

--- src/Fever_Medicine_Recommendation/constants/__init__.py
+++ src/Fever_Medicine_Recommendation/constants/__init__.py
@@ -4,6 +4,3 @@ from pathlib import Path
 CONFIG_FILE_PATH = Path('config\config.yaml')
 PARAMS_FILE_PATH = Path('params.yaml')
 SCHEMA_FILE_PATH = Path('schema.yaml')
-DATABASE_NAME =str("Fever_Medication")
-COLLECTION_NAME = str("Medication_Data")
-uri = str("mongodb+srv://alumarobin:OdnfKqhBfAVNIl4S@cluster0.nnizo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
--- src/Fever_Medicine_Recommendation/utils/common.py
+++ src/Fever_Medicine_Recommendation/utils/common.py
@@ -1,6 +1,8 @@
 from box import ConfigBox
 import yaml
 import os
+from pathlib import Path
+from ensure import ensure_annotations
 from logger import fever_logger
 def read_yaml(filepath)->ConfigBox:
     with open(filepath) as yaml_file:
@@ -12,3 +14,9 @@ def create_directories(path_to_directories:list):
         os.makedirs(path,exist_ok=True)
         
         fever_logger.info(f"Created directory at path: {path}")
+
+
+@ensure_annotations
+def save_data(filepath:Path):
+    with open(filepath, 'w') as data:
+        data.write()
